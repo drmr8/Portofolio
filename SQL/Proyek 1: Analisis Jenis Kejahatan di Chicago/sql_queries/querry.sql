4c33b87c6ab44f6ec300291488268121d72e043b
drmr <108063049+drmr8@users.noreply.github.com>
2023-05-25 13:18:22 +0800

    Update querry.sql

--- SQL/Proyek 1: Analisis Jenis Kejahatan di Chicago/sql_queries/querry.sql
+++ SQL/Proyek 1: Analisis Jenis Kejahatan di Chicago/sql_queries/querry.sql
@@ -8,3 +8,29 @@ GROUP BY
   primary_type
 ORDER BY 
   count DESC;
+
+2. Mengidentifikasi Tren Kejahatan Berdasarkan Tahun
+SQL Query:
+SELECT 
+  year, 
+  COUNT(*) as count
+FROM 
+  `bigquery-public-data.chicago_crime.crime` 
+GROUP BY 
+  year
+ORDER BY 
+  year;
+ 
+3. Menentukan Lokasi Kejahatan Terbanyak
+SQL Query:
+SELECT 
+  location_description,
+  COUNT(*) as count
+FROM 
+  `bigquery-public-data.chicago_crime.crime` 
+WHERE 
+  location_description IS NOT NULL
+GROUP BY 
+  location_description
+ORDER BY 
+  count DESC
